3839dc07e5af946214ee35d858d9432055d5c902
xgqfrms <xgqfrms@outlook.com>
2019-02-25 17:20:10 +0800

    no .DS_Store

--- next-demo-app/pages/index.js
+++ next-demo-app/pages/index.js
@@ -18,13 +18,13 @@ import Link from "next/link";
 const Index = () => (
     <section>
         <div>
-            <Link href="/about" style={{fontSize: 20,}}>
+            {/* <Link href="/about" style={{fontSize: 20,}}>
                 <a>About Page</a>
+            </Link> */}
+            <Link href="/about">
+                <a style={{fontSize: 20,}}>About Page</a>
             </Link>
             <Link href="/about">
-                <a style={{ fontSize: 20 }}>About Page</a>
-            </Link>
-            <Link href="/about" style={{fontSize: 20,}}>
                 <button>Go to About Page</button>
             </Link>
         </div>
