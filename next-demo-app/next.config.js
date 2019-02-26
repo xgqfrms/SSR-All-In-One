036b2cd648f162f22159999998cc9fd5312e5f03
xgqfrms <xgqfrms@outlook.com>
2019-02-26 23:29:42 +0800

    npm run export

---
+++ next-demo-app/next.config.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ *
+ * @created 2018.02.26
+ * @modified 2018.02.26
+ *
+ * @description
+ * @augments
+ * @example
+ *
+ */
+
+module.exports = {
+    exportPathMap: function () {
+        return {
+            "/": {
+                page: "/",
+            },
+        };
+    },
+};
